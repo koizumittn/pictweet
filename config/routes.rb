3a8dd9ea5f0fe9405cb7beb7e1b34019417db6eb
Tetsunosuke Koizumi <tetsu@TetsunosukenoMacBook-Pro.local>
2017-04-06 04:56:45 +0900

    usersコントローラ、マイページを作成。

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get   'tweets'      =>  'tweets#index'
   get   'tweets/new'  =>  'tweets#new'
   post  'tweets'      =>  'tweets#create'
+  get   'users/:id'   =>  'users#show'
 
 end
