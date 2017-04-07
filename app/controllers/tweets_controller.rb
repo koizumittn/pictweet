33e2f346351546d819a42ae6179de6285b560a07
Tetsunosuke Koizumi <tetsu@TetsunosukenoMacBook-Pro.local>
2017-04-07 20:07:32 +0900

    ツイート更新機能を実装。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,17 @@ class TweetsController < ApplicationController
     Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
   def destroy
     tweet = Tweet.find(params[:id])
     if tweet.user_id == current_user.id
