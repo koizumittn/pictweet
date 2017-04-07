2f28c6f2037341fb5b335f7a9be8601b7d336c84
Tetsunosuke Koizumi <tetsu@TetsunosukenoMacBook-Pro.local>
2017-04-07 19:49:37 +0900

    ツイート削除機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get   'tweets'      =>  'tweets#index'
   get   'tweets/new'  =>  'tweets#new'
   post  'tweets'      =>  'tweets#create'
+  delete  'tweets/:id' => 'tweets#destroy'
   get   'users/:id'   =>  'users#show'
 
 end
